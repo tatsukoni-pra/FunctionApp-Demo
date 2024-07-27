1b808673c457b7ca2e21dc12be6ad26d8547b5fd
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-07-27 20:09:18 +0900

    feat: コード修正

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -2,11 +2,15 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import v8 = require('v8');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const totalHeapSize = v8.getHeapStatistics().total_available_size;
-    const totalHeapSizeinMB = (totalHeapSize /1024/1024).toFixed(2);
-    const totalHeapSizeinGB = (totalHeapSize /1024/1024/1024).toFixed(2);
-    context.log(`Total heap size: ${totalHeapSizeinMB} MB`);
-    context.log(`Total heap size: ${totalHeapSizeinGB} GB`);
+    // const totalHeapSize = v8.getHeapStatistics().total_available_size;
+    // const totalHeapSizeinMB = (totalHeapSize /1024/1024).toFixed(2);
+    // const totalHeapSizeinGB = (totalHeapSize /1024/1024/1024).toFixed(2);
+    // context.log(`Total heap size: ${totalHeapSizeinMB} MB`);
+    // context.log(`Total heap size: ${totalHeapSizeinGB} GB`);
+    context.res = {
+        status: 200,
+        body: `Hello World!`
+    };
 };
 
 export default httpTrigger;
